a9089d82b51342f118a45f0c9f9754104d4569f7
SiahBatterson <51545507+SiahBatterson@users.noreply.github.com>
2025-06-19 13:09:43 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -3,6 +3,8 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+#Testing
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pets.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
